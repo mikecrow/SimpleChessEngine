03814f075332bb90eb63eb9a0e1efa1333465ffd
Mikołaj Wrona <mikolaj-wrona@wp.pl>
2021-01-25 21:50:36 +0100

    working opening book, and added alpha beta pruning

--- engine.py
+++ engine.py
@@ -1,4 +1,5 @@
 import chess
+import chess.polyglot
 
 piece_values = {
     chess.PAWN: 100,
@@ -11,10 +12,6 @@ piece_values = {
 
 
 def evaluate(board):
-    if board.result() == '1-0':
-        return float('inf')
-    elif board.result() == '0-1':
-        return -float('inf')
 
     white_material = 0
     black_material = 0
@@ -30,35 +27,71 @@ def evaluate(board):
     # TODO better eval
 
 
-def minimax(board, depth, maximizing_player):
-    if depth == 0 or board.is_game_over():
+def minimax(board, depth, alpha, beta, maximizing_player, moves):
+    if board.is_game_over():
+        if board.result() == '1-0':
+            return 100000-moves
+        elif board.result() == '0-1':
+            return -100000+moves
+        elif board.result() == '1/2-1/2':
+            return 0
+
+    if depth == 0:
         return evaluate(board)
+
     if maximizing_player:
         value = -float('inf')
         for move in board.legal_moves:
             board.push(move)
-            value = max(value, minimax(board, depth - 1, False))
+            value = max(value, minimax(board, alpha, beta, depth - 1, False, moves+1))
             board.pop()
+            if beta <= alpha:
+                return value
         return value
     else:
         value = float('inf')
         for move in board.legal_moves:
             board.push(move)
-            value = min(value, minimax(board, depth - 1, True))
+            value = min(value, minimax(board, alpha, beta, depth - 1, True, moves+1))
             board.pop()
+            if beta <= alpha:
+                return value
         return value
-    # TODO alpha beta
 
 
-def choose_move(board, depth):
-    value = -float('inf')
-    best_move = None
-    for move in board.legal_moves:
-        board.push(move)
-        new_value = minimax(board, depth, False)
-        board.pop()
-        if new_value > value:
-            best_move = move
-            value = new_value
+def make_a_move(board, depth, maximizing_player):
 
-    return best_move
+    with chess.polyglot.open_reader("./DCbook_large.bin") as reader:
+        try:
+            book_move = reader.weighted_choice(board).move
+            return book_move
+        except IndexError:
+            pass
+
+    if maximizing_player:
+        value = -float('inf')
+        best_move = None
+        for move in board.legal_moves:
+            board.push(move)
+            new_value = minimax(board, depth - 1, -float('inf'), float('inf'), False, 1)
+            board.pop()
+            if new_value >= value:
+                best_move = move
+                value = new_value
+                # print(move, value)
+
+        return best_move
+    else:
+        value = float('inf')
+        best_move = None
+        for move in board.legal_moves:
+            board.push(move)
+            new_value = minimax(board, depth - 1, -float('inf'), float('inf'), True, 1)
+            board.pop()
+            if new_value <= value:
+                best_move = move
+                value = new_value
+                # print(move, value)
+
+        return best_move
+
--- main.py
+++ main.py
@@ -1,19 +1,56 @@
 import chess
+import chess.polyglot
+import random
 import engine
 
 board = chess.Board()
+#board = chess.Board('q1r4k/6pp/8/6N1/8/1Q6/6PP/7K w - - 0 1')
+#board = chess.Board('5k2/8/4K3/8/8/8/2R5/8 w - - 0 1')
+#board = chess.Board('5q2/8/8/8/8/6k1/8/7K w - - 0 1')
 
 
 def main():
+
+    # board = chess.Board('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1')
+    # print(board)
+    # with chess.polyglot.open_reader("./DCbook_large.bin") as reader:
+    #     print(random.choice(list(reader.find_all(board))).move)
+    #     print("soaiodpaiaspodisapoidopasidpoaip\n")
+    #
+    #     for entry in reader.find_all(board):
+    #         print(entry.move)
 
     # board.push_san("e4")
     # board.push_san("e5")
     # board.push_san("Qh5")
     # board.push_san("g6") # blunder
 
-    while not board.is_game_over():
+    while True:
         print(board)
-        move = engine.choose_move(board, 3)
+        print('\n ------------------ \n')
+
+        if board.is_game_over():
+            print('koniec gry :)')
+            print(board.result())
+            break
+
+        human_move = input('proszę podaj ruch \n')
+        board.push_san(human_move)
+
+        #move = engine.make_a_move(board, 4, True)
+        #board.push(move)
+
+
+        print(board)
+        print('\n ------------------ \n')
+
+        if board.is_game_over():
+            print('koniec gry :)')
+            print(board.result())
+            break
+
+        #human_move = input('proszę podaj ruch \n')
+        move = engine.make_a_move(board, 4, False)
         board.push(move)
 
 
